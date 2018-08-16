60cad311b9391054efeb69cb8c4f5e71e98e9f82
eduardovaloisfranz <dudufranz13@gmail.com>
2018-08-16 17:21:38 -0300

    clienteRepositorio concluido

--- TrabalhoTcc/TrabalhoTcc/Repositorio/ClientesRepositorio.cs
+++ TrabalhoTcc/TrabalhoTcc/Repositorio/ClientesRepositorio.cs
@@ -46,7 +46,47 @@ namespace TrabalhoTcc.Repositorio
             comando.Parameters.AddWithValue("@TELEFONE", cliente.Telefone);
             int id = Convert.ToInt32(comando.ExecuteScalar().ToString());
             return id;
-        }        
-        
+        }            
+        public bool Alterar(Cliente cliente)
+        {
+            SqlCommand comando = new BancoDados().ObterConexao();
+            comando.CommandText = @"UPDATE alunos SET
+nome = @NOME,
+celular = @CELULAR,
+telefone = @TELEFONE
+WHERE id = @ID";
+            comando.Parameters.AddWithValue("@NOME", cliente.Nome);
+            comando.Parameters.AddWithValue("@CELULAR", cliente.Celular);
+            comando.Parameters.AddWithValue("@TELEFONE", cliente.Telefone);
+            comando.Parameters.AddWithValue("@ID", cliente.Id);
+            return comando.ExecuteNonQuery() == 1;
+        }
+        public bool Excluir(int id)
+        {
+            SqlCommand comando = new BancoDados().ObterConexao();
+            comando.CommandText = "DELETE from CLIENTES WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            return comando.ExecuteNonQuery() == 1;
+        }
+
+        public Cliente ObterPeloId(int id)
+        {
+            Cliente cliente = null;
+            SqlCommand comando = new BancoDados().ObterConexao();
+            comando.CommandText = "SELECT nome, data_nascimento celular, telefone  WHERE id = @ID";
+            comando.Parameters.AddWithValue("@ID", id);
+            DataTable tabela = new DataTable();
+            tabela.Load(comando.ExecuteReader());
+            if (tabela.Rows.Count == 1)
+            {
+                cliente = new Cliente();
+                cliente.Id = id;
+                cliente.Nome = tabela.Rows[0][0].ToString();
+                cliente.Data_nascimento = Convert.ToDateTime(tabela.Rows[0][1].ToString());
+                cliente.Celular = tabela.Rows[0][2].ToString();
+                cliente.Telefone = tabela.Rows[0][3].ToString();
+            }
+            return cliente;
+        }
     }
 }
