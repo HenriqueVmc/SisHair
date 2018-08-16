703f95d544ff2cbfea3a63fcb2fc341ef954a9cb
Adrian Boaventura Silva <adrianboaventura391@gmail.com>
2018-08-16 14:34:56 -0300

    .

--- TrabalhoTcc/TrabalhoTcc/Models/LoginFuncionario.cs
+++ TrabalhoTcc/TrabalhoTcc/Models/LoginFuncionario.cs
@@ -7,5 +7,16 @@ namespace TrabalhoTcc.Models
 {
     public class LoginFuncionario
     {
+   
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string CPF { get; set; }
+        public string CTPS { get; set; }
+        public string Cargo { get; set; }
+        public string Contato { get; set; }
+        public string Endereco { get; set; }
+
     }
 }
