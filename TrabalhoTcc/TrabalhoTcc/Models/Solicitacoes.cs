c03c34e94263b622e6c0a3cbfc9db8f744591d35
eduardovaloisfranz <dudufranz13@gmail.com>
2018-08-17 13:43:05 -0300

    validacao solicitacao

--- TrabalhoTcc/TrabalhoTcc/Models/Solicitacoes.cs
+++ TrabalhoTcc/TrabalhoTcc/Models/Solicitacoes.cs
@@ -11,9 +11,11 @@ namespace TrabalhoTcc.Models
         public int Id { get; set; }
 
         public DateTime Hora_inicio { get; set; }
-
+        
         public DateTime Data { get; set; }
-
+        [Required(ErrorMessage="Situacao deve ser preenchida")]
+        [MinLength(7, ErrorMessage="Situacao deve conter no mínimo 7 caracteres")]
+        [MaxLength(50, ErrorMessage="Solicitacao nao deve exceder 50 caracteres")]
         public string Situacao { get; set; }
     
     }
