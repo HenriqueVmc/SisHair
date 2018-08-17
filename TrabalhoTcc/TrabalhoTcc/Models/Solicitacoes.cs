d204805f845a18c0c9738331b91ae57168c16440
eduardovaloisfranz <dudufranz13@gmail.com>
2018-08-17 13:38:50 -0300

    Classe solicitacoes

---
+++ TrabalhoTcc/TrabalhoTcc/Models/Solicitacoes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrabalhoTcc.Models
+{
+    public class Solicitacoes
+    {
+        public int Id { get; set; }
+
+        public DateTime Hora_inicio { get; set; }
+
+        public DateTime Data { get; set; }
+
+        public string Situacao { get; set; }
+    
+    }
+}
