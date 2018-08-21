51d7eb29a293d0bf84178d1974d305440f8588b1
Adrian Boaventura Silva <adrianboaventura391@gmail.com>
2018-08-21 14:07:52 -0300

    .

--- TrabalhoTcc/TrabalhoTcc/Models/Endereco.cs
+++ TrabalhoTcc/TrabalhoTcc/Models/Endereco.cs
@@ -9,12 +9,20 @@ namespace TrabalhoTcc.Models
     public class Endereco
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="A rua não pode ser vasia")]
         public string Rua { get; set; }
+        [Required(ErrorMessage = "O bairro não pode ser vasio")]
         public string Bairro { get; set; }
+        [Required(ErrorMessage = "O número da casa não pode ser vasio")]
         public string NumeroDaCasa { get; set; }
         public string Complemento { get; set; }
+        [Required(ErrorMessage = "O estado não pode ser vasio")]
         public string Estado { get; set; }
+        [Required(ErrorMessage = "A cidada não pode ser vasia")]
         public string Cidade { get; set; }
+        [Required(ErrorMessage = "O CEP não pode ser vasio")]
+        [MinLength(8, ErrorMessage="CEP inválido")]
+        [MaxLength(8,ErrorMessage="CEP inválido")]
         public string CEP { get; set; }
 
 
