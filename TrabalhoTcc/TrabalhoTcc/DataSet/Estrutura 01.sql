b62c7233192888b6b03f0b5bb294c0dcc0059cbb
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-09-04 02:18:35 -0300

    Link Index Arrumado area cliente

---
+++ TrabalhoTcc/TrabalhoTcc/DataSet/Estrutura 01.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO Clientes (Nome, Data_nascimento, Celular, Telefone, Email)
+VALUES ('Alan', '2000-05-05', '479998956', '4566666666', 'alaneduardoalves2018@gmail.com')
+
+INSERT INTO Cargoes (Nome) VALUES ('Cabeleireiro');
+INSERT INTO Funcionarios (CargoId, Nome, DataNascimento, Telefone, Cpf) VALUES (1,'Administrador', '1999-09-09', '11991578765', '98798798776');
+INSERT INTO LoginFuncionarios (FuncionarioId, Usuario, Senha ) VALUES (1, 'adm', 'adm');
+
+SELECT * FROM Clientes
+SELECT * FROM LoginClientes
+
+INSERT INTO LoginClientes (Usuario, Senha, ClienteId) 
+VALUES ('Alanedu2000', '123456', 1)
+
+
+
+
+
+
+SELECT * FROM CodigoClientes
+DELETE FROM CodigoClientes WHERE Id = 9
