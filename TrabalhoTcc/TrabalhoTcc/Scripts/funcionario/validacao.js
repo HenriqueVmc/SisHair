70b5750c757175852d0fa029e9e00771a114053a
EduardoValoisFranz <38699565+eduardovaloisfranz@users.noreply.github.com>
2018-09-12 00:19:41 -0300

    Preenchimento de endereço com base no CEP e validacao CPF
    Preenchimento de endereço:  OK
    Validacao CPF: não concluido

---
+++ TrabalhoTcc/TrabalhoTcc/Scripts/funcionario/validacao.js
@@ -0,0 +1,36 @@
+﻿
+//$(document).ready(function(){
+//    $("#formCadastroFuncionario").validate({
+//        rules: {
+//            Cpf: {                
+//                cpfBR: true
+//            },
+//            Nome: {
+//                minwords: 2
+//            }
+//        }
+//    })
+//})
+
+$(document).ready(function () {
+    $("#formCadastroFuncionario").validate({
+        rules: {
+            Nome: {
+                required: true,
+                maxlength: 100,
+                minlength: 10,
+                minWords: 2
+            },
+            Cpf: {
+                cpfBR: true
+            },
+            Celular: {
+                minlength: 8,
+                maxlength: 100
+            }
+
+        }
+
+
+    })
+})
