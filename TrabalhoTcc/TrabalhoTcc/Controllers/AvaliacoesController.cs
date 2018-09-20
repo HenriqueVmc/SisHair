425723b9b3c3722bab629f5ea34f6bc077b6f5a6
Adrian Boaventura Silva <adrianboaventura391@gmail.com>
2018-09-20 17:30:59 -0300

    .

---
+++ TrabalhoTcc/TrabalhoTcc/Controllers/AvaliacoesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrabalhoTcc.Context;
+using TrabalhoTcc.Models;
+using Newtonsoft.Json;
+using TrabalhoTcc.Models.Conta;
+using System.Web.UI.WebControls;
+using System.IO;
+using System.Web.UI;
+using System.Net.Mail;
+
+namespace TrabalhoTcc.Controllers
+{
+    public class AvaliacoesController : Controller
+    {
+        private DBContext db = new DBContext();
+        public ActionResult Index()
+        {
+            var result = db.Avaliacoes.Include(a => a.Agendamento.Cliente).ToList();
+            
+            
+            ViewBag.Avaliacoes = result;
+            
+            
+            return View();
+
+        }
+    }
+
+
+}
